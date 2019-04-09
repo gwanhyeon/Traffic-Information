d06f3f38c163eb6b40ddd610575f412a3823daf1
kgh940525 <kgh940525@gmail.com>
2019-04-10 03:31:42 +0900

    React-redux connected to modules(action type, action Func, Reducer)

--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,24 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// 스토어 생성 리덕스
+
+import modules from './modules';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+
+
+
+const store = createStore(modules,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
+// App.js Provider를 통하여 리액트 프로젝트에 적용시키기
+ReactDOM.render(
+    <Provider store={store}>
+    <App />
+    </Provider>, 
+    document.getElementById('root'));
+
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
