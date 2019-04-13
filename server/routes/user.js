242f32f9e8e96fc994585edc4d763b3bdc47d86c
kgh940525 <kgh940525@gmail.com>
2019-04-14 03:22:50 +0900

    [server router] Route설정 및 route my_profile config strategy

--- server/routes/user.js
+++ server/routes/user.js
@@ -102,6 +102,12 @@ router.post('/login',(req,res) => {
                 }
             })
     })
-
 })
-
+router.get('/me',passport.authenticate('jwt',{session:false}),(req,res)=>{
+    return res.json({
+        id: req.user.id,
+        name: req.user.name,
+        email: req.user.email
+    });
+});
+module.exports = router;
