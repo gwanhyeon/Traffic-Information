5341c2a30719f7a900ddb516347e75c46edd0bdc
kgh940525 <kgh940525@gmail.com>
2019-04-21 03:39:54 +0900

    [client] redux-thunk aplly to store.js

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -8,72 +8,81 @@ import axios from 'axios';
 class Signup extends Component {
 
     // 1. 값의 변화 상태 감지 핸들
-    componentDidMount(){
-        const url = 'http://swopenAPI.seoul.go.kr/api/subway/6a7644634e6b67683739434e557a61/xml/realtimeStationArrival'
-        let location = "의왕";
-        const subway_id='1';
-        const stain_id = '52';
-        const attribute_length = 22;
-        let row_length = null;
-        location = encodeURIComponent(location);        //인코딩한 값 넣어주기
+    // componentDidMount(){
+    //     const url = 'http://swopenAPI.seoul.go.kr/api/subway/6a7644634e6b67683739434e557a61/xml/realtimeStationArrival'
+    //     let location = "의왕";
+    //     const subway_id='1';
+    //     const stain_id = '52';
+    //     const attribute_length = 22;
+    //     let row_length = null;
+    //     location = encodeURIComponent(location);        //인코딩한 값 넣어주기
 
-        // ,{responseType : 'xml'}
-        axios.get(`${url}/${subway_id}/${stain_id}/${location}`)
-        .then(res =>{
+    //     // ,{responseType : 'xml'}
+    //     axios.get(`${url}/${subway_id}/${stain_id}/${location}`)
+    //     .then(res =>{
 
 
-            console.log('res data =>',res.data);
+    //         console.log('res data =>',res.data);
 
-            var parser = new DOMParser(),
-            xmlDoc = parser.parseFromString(res.data,'text/xml');
+    //         var parser = new DOMParser(),
+    //         xmlDoc = parser.parseFromString(res.data,'text/xml');
             
-            row_length = xmlDoc.getElementsByTagName('row').length;
-            // console.log("row 개수 ", xmlDoc.getElementsByTagName('row')[0].getAttribute.length);
+    //         row_length = xmlDoc.getElementsByTagName('row').length;
+    //         // console.log("row 개수 ", xmlDoc.getElementsByTagName('row')[0].getAttribute.length);
             
-            for(var i=0; i< row_length; i++){
-                for(var j=0; j<attribute_length; j++){
-                console.log("hello xml",xmlDoc.getElementsByTagName('row')[i]
-                .childNodes[j].childNodes[0])
-                }
-                console.log("여기잠시대기")
-            }
+    //         for(var i=0; i< row_length; i++){
+    //             for(var j=0; j<attribute_length; j++){
+    //             console.log("hello xml",xmlDoc.getElementsByTagName('row')[i]
+    //             .childNodes[j].childNodes[0])
+    //             }
+    //             console.log("여기잠시대기")
+    //         }
            
             
-            //todo firstChild -> 해당 태그를 가져온다.
-            //todo 
+    //         //todo firstChild -> 해당 태그를 가져온다.
+    //         //todo 
         
-        })
-        
-    
+    //     })
+    // }
+
+
+    // AUTH Async HTTP Connection 
+    componentDidMount() {
+      axios.post('http://localhost:3002/user/signup')
+      .then(res => {
+          console.log(res);
+      })
     }
+    
+
     handleChange = (e) =>{
         const {signupActions} = this.props;
-        const {user_id,user_password,user_name, result} = this.props;
+        const {user_id,user_password,user_name, auth} = this.props;
         console.log("SignupHandle");
         console.log(user_id);
         console.log(user_password);
         console.log(user_name);
-        console.log(result);
+        console.log(auth);
         signupActions.showSignup({
-            'user_id':e.target.name,
-            'user_password':e.target.value,
-            'user_name' : e.target.user_name,
-            'result': ''
+            user_id:e.target.name,
+            user_password:e.target.value,
+            user_name : e.target.user_name,
+            auth : false
 
-    })
-        // signupActions.showSignup({'property':e.target.name,'value':e.target.value});
+         })
+        signupActions.showSignup({'property':e.target.name,'value':e.target.value});
     }
 
     // 2. 회원가입 제출 핸들
     handleSubmit = (e) =>{
         e.preventDefault();
         const {signupActions} = this.props;
-        const {user_id,user_password,user_name, result} = this.props;
+        const {user_id,user_password,user_name, auth} = this.props;
         const user = {
             user_id : user_id,
             user_password: user_password,
             user_name : user_name,
-            result : "200 OK"
+            auth : auth
         }
         console.log("user=>" + user);
         signupActions.setSignup(user);
@@ -110,11 +119,12 @@ class Signup extends Component {
 export default connect(
     // 리덕스랑 연결이 되면 그 값들을 
     // user_id , user_password, user_name , result 값으로 세팅을 한다
+    
     (state) => ({
         user_id : state.Signup.get('user_id'),
         user_password : state.Signup.get('user_password'),
         user_name : state.Signup.get('user_name'),
-        result : state.Signup.get('result')
+        auth : state.Signup.get('auth')
     }),
     (dispatch) =>({
         signupActions: bindActionCreators(signupActions,dispatch)
--- client/src/index.js
+++ client/src/index.js
@@ -7,12 +7,12 @@ import * as serviceWorker from './serviceWorker';
 // 스토어 생성 리덕스
 
 import modules from './modules';
-import {createStore} from 'redux';
+import {createStore, applyMiddleware, compose} from 'redux';
 import {Provider} from 'react-redux';
+import ReduxThunk from 'redux-thunk';
 
-
-
-const store = createStore(modules,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+// compose를 통한 ReduxThunk 연결 
+const store = createStore(modules,compose(applyMiddleware(ReduxThunk),window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()));
 
 // App.js Provider를 통하여 리액트 프로젝트에 적용시키기
 ReactDOM.render(
@@ -20,9 +20,6 @@ ReactDOM.render(
     <App />
     </Provider>, 
     document.getElementById('root'));
-
-
-
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
