316bb196c75dd8e9a224916402d0e73bbc3d25c9
so2ming <so2_cloud@naver.com>
2019-05-18 18:06:02 +0900

    [server] 리스트 find() 함수 추가

--- server/routes/route_board_list.js
+++ server/routes/route_board_list.js
@@ -5,7 +5,7 @@ const Board = require('../models/model_board');
 
 /* board find by id */
 router.get('/board', function (req, res) {
-    console.log("board connect");
+    // console.log("board connect");
     Board.findOne({
         id: req.params.id
     }).then(user =>{
@@ -20,9 +20,7 @@ router.get('/board_list', function (req, res) {
     Board.find().then(user =>{
         if(user){
             res.send(user);
-            console.log("find");
         }
-
     })
 });
 
