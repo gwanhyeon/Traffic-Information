f4c97af4dd87a72c20dc7977f9a8a860f5080dc3
so2ming <so2_cloud@naver.com>
2019-05-28 18:51:51 +0900

    [client] board 날짜 뿌려주기, 수정하기

--- client/src/components/BoardItem.js
+++ client/src/components/BoardItem.js
@@ -1,4 +1,5 @@
 import React, { Fragment,Component } from 'react';
+import BoardRead from './BoardRead';
 // import Fragment from 'Fragment';
 class BoardItem extends Component {
     state = {
@@ -7,87 +8,44 @@ class BoardItem extends Component {
         board_contents:''
     }
     handleRead = () => {
-      const {board_id, onRead} = this.props;
-      onRead(board_id);
+      const { reading } = this.state;
+      this.setState({ reading : !reading });
   }
     handleRemove = () => {
         const {board_id, onRemove} = this.props;
         onRemove(board_id);
     }
-    handleToggleEdit = () => {
-        const { editing } = this.state;
-        this.setState({ editing: !editing });
-    }
-    handleChange = (e) => {
-        const {name, value} = e.target;
-        this.setState({
-            [name]: value
-        })
-    }
 
-    componentDidUpdate(prevProps, prevState) {
-        // 여기서는, editing 값이 바뀔 때 처리 할 로직이 적혀있습니다.
-        // 수정을 눌렀을땐, 기존의 값이 input에 나타나고,
-        // 수정을 적용할땐, input 의 값들을 부모한테 전달해줍니다.
-    
-        const { board_title, board_contents, onUpdate, board_id } = this.props;
-        if(!prevState.editing && this.state.editing) {
-          // editing 값이 false -> true 로 전환 될 때
-          // info 의 값을 state 에 넣어준다
-          this.setState({
-            board_title: board_title,
-            board_contents: board_contents
-          })
-        }
-    
-        if (prevState.editing && !this.state.editing) {
-          // editing 값이 true -> false 로 전환 될 때
-          onUpdate(board_id, {
-            board_title: this.state.board_title,
-            board_contents: this.state.board_contents
-          });
-        }
-      }
     render() {
         const {board_id, board_title,board_contents,board_date,board_author} = this.props;
-        const {editing} = this.state;
+        const {editing, reading} = this.state;
 
-        if (editing) { // 수정모드
-            return (
-                <Fragment>
+        if(reading) {
+          return (
+            <Fragment>
                 <tr style={{fontFamily: 'sans-serif', fontSize: '1.5vw'}}>
                 <td>{board_id}</td>
-                <td> <input
-                    value={this.state.board_title}
-                    name="board_title"
-                    placeholder="제목"
-                    onChange={this.handleChange}
-                  /> </td>
-                <td> <input
-                    value={this.state.board_contents}
-                    name="board_contents"
-                    placeholder="내용"
-                    onChange={this.handleChange}
-                  /> </td>
+                <td onClick={this.handleRead}> {board_title} </td>
+                {/* <td> {board_contents} </td> */}
                 <td> {board_author}</td>
                 <td> {board_date} </td>
-                <td> <button onClick={this.handleToggleEdit} className="btn btn-primary" style={{fontFamily: 'sans-serif', fontSize: '1.5vw'}}>적용</button></td>
-                <td> <button onClick={this.handleRemove} className="btn btn-primary" style={{fontFamily: 'sans-serif', fontSize: '1.5vw'}}>삭제</button></td>
-                
+                <td> <button  className="btn btn-primary" style={{fontFamily: 'sans-serif', fontSize: '1.5vw'}}>삭제</button></td>
                 </tr>
-                </Fragment>
-            );
-          }
-        
+                <tr>
+                  <td colSpan="7"> <BoardRead dataFromParent={board_id} onUpdate={this.handleUpdate} /> </td>
+                </tr>
+            </Fragment>
+          );
+        }
+
         return (
                 <Fragment>
                 <tr style={{fontFamily: 'sans-serif', fontSize: '1.5vw'}}>
                 <td>{board_id}</td>
                 <td onClick={this.handleRead}> {board_title} </td>
-                <td> {board_contents} </td>
+                {/* <td> {board_contents} </td> */}
                 <td> {board_author}</td>
                 <td> {board_date} </td>
-                <td> <button onClick={this.handleToggleEdit} className="btn btn-primary" style={{fontFamily: 'sans-serif', fontSize: '1.5vw'}}>수정</button></td>
                 <td> <button onClick={this.handleRemove} className="btn btn-primary" style={{fontFamily: 'sans-serif', fontSize: '1.5vw'}}>삭제</button></td>
                 
                 </tr>
