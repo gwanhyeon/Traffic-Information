81e8b03bbb0e9c593215dfab87dede9610cb4deb
jun971006 <wnstjrdlrj@gmail.com>
2019-06-02 04:58:16 +0900

    [client]App.js-add AboutInfo path, About.js-add 자세히보기 버튼,  add AboutInfo.js

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -5,24 +5,27 @@ import image1 from './images/photo.png'
 import jun from './images/jun.jpeg'
 import so from './images/so.jpg'
 import ji from './images/ji.jpg'
-
+import { Link } from 'react-router-dom';
+import AboutInfo from './AboutInfo';
 class About extends Component{
     render(){
         return(
             <div className = 'container-fluid row justify-content-around ' style={{color:'black'}}>
             <div class="card shadow-lg" style={{height: 'auto',width: '18rem', marginTop: '1rem'}}>
-            <img src={gwan} class="card-img-top" alt="..." style={{height: '360px', width:'auto'}}/>
+            <img src={gwan} class="card-img-top" alt="..." style={{height: '360px', width:'auto'}} />
             <div class="card-body" >
               <h2 class="card-title">김 관 현<h5>(Kim Gwan-hyeon)</h5> </h2>
               <p class="card-text">한국교통대학교<br/>컴퓨터정보공학과</p>
+              <button class="btn btn-primary"  style={{fontSize:'0.96rem'}}><Link to="/AboutInfo/gwan">자세히보기</Link></button><br/>
               <a href="https://github.com/kgh940525/" class="btn btn-danger" style={{fontSize:'1rem'}}>Github</a>
             </div>
             </div>
             <div class="card shadow-lg" style={{height: 'auto',width: '18rem', marginTop: '1rem'}}>
-            <img src={jun} class="card-img-top" alt="..." style={{height: '360px', width:'auto'}}/>
+            <img src={jun} class="card-img-top" alt="..." style={{height: '360px', width:'auto'}} />
             <div class="card-body">
-              <h2 class="card-title">이 준 석<h5>(Lee Jun-seok)</h5></h2>
+              <h2 class="card-title" onClick={<AboutInfo name="이준석"/>}>이 준 석<h5>(Lee Jun-seok)</h5></h2>
               <p class="card-text">한국교통대학교<br/>컴퓨터정보공학과</p>
+              <button class="btn btn-primary"  style={{fontSize:'0.96rem'}}><Link to="/AboutInfo/jun">자세히보기</Link></button><br/>
               <a href="https://github.com/jun971006" class="btn btn-danger" style={{fontSize:'1rem'}}>Github</a>
             </div>
             </div>
@@ -31,6 +34,7 @@ class About extends Component{
             <div class="card-body">
               <h2 class="card-title">김 소 의<h5>(Kim So-ui)</h5></h2>
               <p class="card-text">한국교통대학교<br/>컴퓨터정보공학과</p>
+              <button class="btn btn-primary"  style={{fontSize:'0.96rem'}}><Link to="/AboutInfo/so">자세히보기</Link></button><br/>
               <a href="https://github.com/so2ming" class="btn btn-danger" style={{fontSize:'1rem'}}>Github</a>
             </div>
             </div>
@@ -39,6 +43,7 @@ class About extends Component{
             <div class="card-body">
               <h2 class="card-title">이 지 현<h5>(Lee Ji-hyeon)</h5></h2>
               <p class="card-text">한국교통대학교<br/>컴퓨터정보공학과</p>
+              <button class="btn btn-primary"  style={{fontSize:'0.96rem'}}><Link to="/AboutInfo/ji">자세히보기</Link></button><br/>
               <a href="https://github.com/easy0319/" class="btn btn-danger" style={{fontSize:'1rem'}}>Github</a>
             </div>
             </div>
