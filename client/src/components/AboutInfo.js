177819fe8e5017ba340b885643dec427082876f8
jun971006 <wnstjrdlrj@gmail.com>
2019-06-02 20:33:09 +0900

    [client]AboutInfo.js-add Info

--- client/src/components/AboutInfo.js
+++ client/src/components/AboutInfo.js
@@ -147,45 +147,45 @@ import gwan from './images/gwan.jpg'
                             </div>
                             </div>
                             <div className="w3-container">
-                            <p><i className="fa fa-briefcase fa-fw w3-margin-right w3-large w3-text-teal"></i>Designer</p>
+                            <p><i className="fa fa-briefcase fa-fw w3-margin-right w3-large w3-text-teal"></i>Student</p>
                             <p><i className="fa fa-home fa-fw w3-margin-right w3-large w3-text-teal"></i>gwangmyeongsi, gyeong-gi</p>
                             <p><i className="fa fa-envelope fa-fw w3-margin-right w3-large w3-text-teal"></i>wnstjrdlrj@gmail.com</p>
-                            <p><i className="fa fa-phone fa-fw w3-margin-right w3-large w3-text-teal"></i>1224435534</p>
+                            <p><i className="fa fa-phone fa-fw w3-margin-right w3-large w3-text-teal"></i>01087560365</p>
                             <hr/>
 
                             <p className="w3-large"><b><i className="fa fa-asterisk fa-fw w3-margin-right w3-text-teal"></i>Skills</b></p>
-                            <p>Adobe Photoshop</p>
-                            <div className="w3-light-grey w3-round-xlarge w3-small">
-                                <div className="w3-container w3-center w3-round-xlarge w3-teal" style={{width:"90%"}}>90%</div>
-                            </div>
-                            <p>Photography</p>
-                            <div className="w3-light-grey w3-round-xlarge w3-small">
-                                <div className="w3-container w3-center w3-round-xlarge w3-teal" style={{width:"80%"}}>
-                                <div className="w3-center w3-text-white">80%</div>
-                                </div>
-                            </div>
-                            <p>Illustrator</p>
-                            <div className="w3-light-grey w3-round-xlarge w3-small">
-                                <div className="w3-container w3-center w3-round-xlarge w3-teal" style={{width:"75%"}}>75%</div>
-                            </div>
-                            <p>Media</p>
+                            <p>C</p>
                             <div className="w3-light-grey w3-round-xlarge w3-small">
                                 <div className="w3-container w3-center w3-round-xlarge w3-teal" style={{width:"50%"}}>50%</div>
+                            </div>
+                            <p>Python</p>
+                            <div className="w3-light-grey w3-round-xlarge w3-small">
+                                <div className="w3-container w3-center w3-round-xlarge w3-teal" style={{width:"30%"}}>
+                                <div className="w3-center w3-text-white">30%</div>
+                                </div>
+                            </div>
+                            <p>Javascript</p>
+                            <div className="w3-light-grey w3-round-xlarge w3-small">
+                                <div className="w3-container w3-center w3-round-xlarge w3-teal" style={{width:"35%"}}>35%</div>
+                            </div>
+                            <p>C++</p>
+                            <div className="w3-light-grey w3-round-xlarge w3-small">
+                                <div className="w3-container w3-center w3-round-xlarge w3-teal" style={{width:"40%"}}>30%</div>
                             </div>
                             <br/>
 
                             <p className="w3-large w3-text-theme"><b><i className="fa fa-globe fa-fw w3-margin-right w3-text-teal"></i>Languages</b></p>
                             <p>English</p>
                             <div className="w3-light-grey w3-round-xlarge">
-                                <div className="w3-round-xlarge w3-teal" style={{height:"24px",width:"100%"}}></div>
+                                <div className="w3-round-xlarge w3-teal" style={{height:"24px",width:"70%"}}></div>
                             </div>
-                            <p>Spanish</p>
+                            <p>Japaness</p>
                             <div className="w3-light-grey w3-round-xlarge">
-                                <div className="w3-round-xlarge w3-teal" style={{height:"24px", width:"55%"}}></div>
+                                <div className="w3-round-xlarge w3-teal" style={{height:"24px", width:"15%"}}></div>
                             </div>
-                            <p>German</p>
+                            <p>Korean</p>
                             <div className="w3-light-grey w3-round-xlarge">
-                                <div className="w3-round-xlarge w3-teal" style={{height:"24px", width:"25%"}}></div>
+                                <div className="w3-round-xlarge w3-teal" style={{height:"24px", width:"100%"}}></div>
                             </div>
                             <br/>
                             </div>
@@ -198,54 +198,47 @@ import gwan from './images/gwan.jpg'
                         <div className="w3-container w3-card w3-white w3-margin-bottom">
                             <h2 className="w3-text-grey w3-padding-16"><i className="fa fa-suitcase fa-fw w3-margin-right w3-xxlarge w3-text-teal"></i>Work Experience</h2>
                             <div className="w3-container">
-                            <h5 className="w3-opacity"><b>Front End Developer / w3schools.com</b></h5>
-                            <h6 className="w3-text-teal"><i className="fa fa-calendar fa-fw w3-margin-right"></i>Jan 2015 - <span className="w3-tag w3-teal w3-round">Current</span></h6>
-                            <p>Lorem ipsum dolor sit amet. Praesentium magnam consectetur vel in deserunt aspernatur est reprehenderit sunt hic. Nulla tempora soluta ea et odio, unde doloremque repellendus iure, iste.</p>
+                            <h5 className="w3-opacity"><b>Front End and Back End Developer</b></h5>
+                            <h6 className="w3-text-teal"><i className="fa fa-calendar fa-fw w3-margin-right"></i>Mar 2019 - <span className="w3-tag w3-teal w3-round">Current</span></h6>
+                            <p>2019년 3월부터 Front End 와 Back End 개발을 진행중입니다.</p>
                             <hr/>
                             </div>
                             <div className="w3-container">
-                            <h5 className="w3-opacity"><b>Web Developer / something.com</b></h5>
-                            <h6 className="w3-text-teal"><i className="fa fa-calendar fa-fw w3-margin-right"></i>Mar 2012 - Dec 2014</h6>
-                            <p>Consectetur adipisicing elit. Praesentium magnam consectetur vel in deserunt aspernatur est reprehenderit sunt hic. Nulla tempora soluta ea et odio, unde doloremque repellendus iure, iste.</p>
-                            <hr/>
+                            <h5 className="w3-opacity"><b>Python Web Project</b></h5>
+                            <h6 className="w3-text-teal"><i className="fa fa-calendar fa-fw w3-margin-right"></i>Mar 2019 - <span className="w3-tag w3-teal w3-round">Current</span></h6>
+                            <p>Aws ec2 + python + flask사용하여 Web개발중입니다.</p><br/>
                             </div>
                             <div className="w3-container">
-                            <h5 className="w3-opacity"><b>Graphic Designer / designsomething.com</b></h5>
-                            <h6 className="w3-text-teal"><i className="fa fa-calendar fa-fw w3-margin-right"></i>Jun 2010 - Mar 2012</h6>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. </p><br/>
+                            <h5 className="w3-opacity"><b>Unix Shell Programming</b></h5>
+                            <h6 className="w3-text-teal"><i className="fa fa-calendar fa-fw w3-margin-right"></i>Mar 2019 - May 2019</h6>
+                            <p>c언어 기반 Unix Shell Programming 진행하였습니다.</p>
+                            <hr/>
                             </div>
                         </div>
 
                         <div className="w3-container w3-card w3-white">
                             <h2 className="w3-text-grey w3-padding-16"><i className="fa fa-certificate fa-fw w3-margin-right w3-xxlarge w3-text-teal"></i>Education</h2>
                             <div className="w3-container">
-                            <h5 className="w3-opacity"><b>W3Schools.com</b></h5>
-                            <h6 className="w3-text-teal"><i className="fa fa-calendar fa-fw w3-margin-right"></i>Forever</h6>
-                            <p>Web Development! All I need to know in one place</p>
+                            <h5 className="w3-opacity"><b>KNUT</b></h5>
+                            <h6 className="w3-text-teal"><i className="fa fa-calendar fa-fw w3-margin-right"></i>2016 - <span className="w3-tag w3-teal w3-round">Current</span></h6>
+                            <p>Korea National University of Transportation</p>
                             <hr/>
                             </div>
                             <div className="w3-container">
-                            <h5 className="w3-opacity"><b>London Business School</b></h5>
+                            <h5 className="w3-opacity"><b>GwangHwi High School</b></h5>
                             <h6 className="w3-text-teal"><i className="fa fa-calendar fa-fw w3-margin-right"></i>2013 - 2015</h6>
-                            <p>Master Degree</p>
+                            <p>Graduated</p>
                             <hr/>
                             </div>
                             <div className="w3-container">
-                            <h5 className="w3-opacity"><b>School of Coding</b></h5>
-                            <h6 className="w3-text-teal"><i className="fa fa-calendar fa-fw w3-margin-right"></i>2010 - 2013</h6>
-                            <p>Bachelor Degree</p><br/>
+                            <h5 className="w3-opacity"><b>Garim Middle School</b></h5>
+                            <h6 className="w3-text-teal"><i className="fa fa-calendar fa-fw w3-margin-right"></i>2010 - 2012</h6>
+                            <p>Graduated</p><br/>
                             </div>
                         </div>
-
-                        
                         </div>
-                        
-                    
                     </div>
-                    
-                    
                     </div>
-
                     <footer className="w3-container w3-teal w3-center w3-margin-top">
                     <p>Find me on social media.</p>
                     <i className="fa fa-facebook-official w3-hover-opacity"></i>
